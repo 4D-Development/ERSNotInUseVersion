ee7ffd982947b5fafa5762de350c077ca9ae0329
Daniel <JDanielO@users.noreply.github.com>
2018-09-27 12:47:05 -0400

    Create project files
    EventRegistrationSystem.Domain: Holds the domain entities and logic; set up for persistence via a repository created with the Entity Framework.
    
    EventRegistrationSystem.WebUI: Holds the controllers and views; acts as the
    UI for the ERS application.
    
    EventRegistrationSystem.UnitTests: Holds the unit tests for the other two projects

---
+++ EventRegistrationSystem/EventRegistrationSystem.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventRegistrationSystem.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventRegistrationSystem.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40ad37be-e15d-44a0-8347-8a385adf67f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
